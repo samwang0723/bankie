a2472ab92789f5945932c796e8ea0dc8c4be5d4e
Sam Wang <sam.wang.0723@gmail.com>
2024-08-19 09:05:28 +0800

    fix: correct outbox column name

--- db/migrations/20240817135606_create_outbox.sql
+++ db/migrations/20240817135606_create_outbox.sql
@@ -1,6 +1,6 @@
 CREATE TABLE outbox (
     id SERIAL PRIMARY KEY,
-    transcaction_id UUID NOT NULL,
+    transaction_id UUID NOT NULL,
     event_type VARCHAR(255) NOT NULL,
     payload JSONB NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
